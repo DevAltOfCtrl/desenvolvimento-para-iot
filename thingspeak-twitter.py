ee59fe0d6a1db9153783cfa0bdab2635e6ebe37c
Mateus Silva <66280847+DevAltOfCtrl@users.noreply.github.com>
2021-10-25 22:55:02 -0300

    Update thingspeak-twitter.py

--- thingspeak-twitter.py
+++ thingspeak-twitter.py
@@ -11,7 +11,7 @@ api = tweepy.API(auth)
 
 
 def idSensor():
-    ids = [1, 2, 3]
+    ids = [0, 1, 2, 3]
     id_monitorado = random.choice(ids)
     return id_monitorado
 
@@ -19,19 +19,24 @@ def idSensor():
 def thingspeak_post():
     threading.Timer(10, thingspeak_post).start()
     id_sensor = idSensor()
-    tweet_string = 'Alarme da região {} acionado'.format(id_sensor)
-    URl = 'https://api.thingspeak.com/update?api_key='
-    KEY = 'QIFF3RFAYVKZXT16'
-    HEADER = '&field1={}'.format(id_sensor)
-    NEW_URL = URl+KEY+HEADER
-    print(NEW_URL)
-    data = urllib.request.urlopen(NEW_URL)
-    print(data)
-    try:
-        api.update_status(tweet_string)
-    except tweepy.TweepError as e:
-        print(e.reason)
+    if id_sensor == 0:
+        print('Valor do field: ', id_sensor)
+    else:
+        tweet_string = 'Alarme da região {} acionado'.format(id_sensor)
+        URl = 'https://api.thingspeak.com/update?api_key='
+        KEY = 'QIFF3RFAYVKZXT16'
+        HEADER = '&field1={}'.format(id_sensor)
+        NEW_URL = URl+KEY+HEADER
+        data = urllib.request.urlopen(NEW_URL)
+        print(data)
+        try:
+            print('Valor do field: ', id_sensor)
+            api.update_status(tweet_string)
+            print('Tweet enviado!')
+        except tweepy.TweepError as e:
+            print(e.reason)
 
 
 if __name__ == '__main__':
     thingspeak_post()
+
