9300fef786326de8873d7eb55eea8a4b2a41351e
Mateus Silva <66280847+DevAltOfCtrl@users.noreply.github.com>
2021-10-25 22:44:07 -0300

    Add files via upload

---
+++ autenticacao.py
@@ -0,0 +1,10 @@
+# Autenticação Tweepy
+# OAuthHandler
+
+API_key = 'eXIjB3Qwbna0efBfGSiDpQEjJ'
+API_key_secret = 'xDf1YE2cZOCeUPILbNU5t1uoYFSLSqUsAOJuHje1NNTFTCcXt9'
+
+# set_acess_token
+
+access_token = '1452777056635400195-DFycOlZ2vGZVTFiWOiwOMfpUZTnKRu'
+access_token_secret = 'KgRHglpQuwVsKJsK7LnPud79fXSl58gRkDQPEcc8juFon'
